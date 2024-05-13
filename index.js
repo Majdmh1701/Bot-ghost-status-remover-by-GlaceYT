31c52967c5be4345844dbc4b4386f57685408cee
Majdmh1701 <87619234+Majdmh1701@users.noreply.github.com>
2024-05-13 17:35:10 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Watching Over CyclesMC","JOIN play.cyclesmc.com];
 
 
 let currentIndex = 0;
